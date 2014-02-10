6d627a5f5f473af9d4d965f2d00c995a5e006c19
Chad Rempp <crempp@gmail.com>
2014-02-09 19:02:44 -0600

    Added loading screen

---
+++ Assets/Game/Loader.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.BuildScreen();
+
+        this.Print("blah");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log ("Planet selected");
+	}
+    
+    private void OnGUI() {
+        
+        
+    }
+
+    private void BuildScreen()
+    {
+        // Grab the texture
+        GameObject bg = GameObject.Find("Background");
+
+        // Center texture
+        bg.guiTexture.pixelInset = new Rect(Screen.width / 2, Screen.height / 2, 0, 0);
+    }
+
+
+    private void Print(string msg)
+    {
+        // Grab the texture
+        GameObject loadList = GameObject.Find("LoadList");
+        GUIText loadListText = loadList.guiText;
+
+        loadListText.text = msg;
+    }
+}
